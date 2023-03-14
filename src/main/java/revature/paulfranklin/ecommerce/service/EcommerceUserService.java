ef7e6386994249a21b16e12e41dcfe1a3a2bf990
Paul Franklin <paul219@revature.net>
2023-03-14 15:15:44 -0400

    Update EcommerceUserService.java

--- src/main/java/revature/paulfranklin/ecommerce/service/EcommerceUserService.java
+++ src/main/java/revature/paulfranklin/ecommerce/service/EcommerceUserService.java
@@ -2,7 +2,24 @@ package revature.paulfranklin.ecommerce.service;
 
 import org.springframework.stereotype.Service;
 
+import revature.paulfranklin.ecommerce.dao.EcommerceUserDAOInterface;
+import revature.paulfranklin.ecommerce.dtos.requests.NewUserRequest;
+import revature.paulfranklin.ecommerce.model.EcommerceUser;
+
 @Service
 public class EcommerceUserService {
+    private final EcommerceUserDAOInterface userRepository;
 
+    public EcommerceUserService(EcommerceUserDAOInterface userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void newUser(NewUserRequest req) {
+        EcommerceUser user = new EcommerceUser();
+        try {
+            userRepository.save(user);
+        } catch (Exception exception) {
+            throw new RuntimeException("Database issue");
+        }
+    }
 }
