ee68feb0c0047e2f3e255b95b495cf93ce8d9ec2
Paul Franklin <paul219@revature.net>
2023-03-14 16:55:59 -0400

    Create EcommerceAuthController.java

---
+++ src/main/java/revature/paulfranklin/ecommerce/controller/EcommerceAuthController.java
@@ -0,0 +1,48 @@
+package revature.paulfranklin.ecommerce.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import revature.paulfranklin.ecommerce.dtos.requests.NewLoginRequest;
+import revature.paulfranklin.ecommerce.dtos.responses.Principal;
+import revature.paulfranklin.ecommerce.model.EcommerceUser;
+import revature.paulfranklin.ecommerce.service.EcommerceTokenService;
+import revature.paulfranklin.ecommerce.service.EcommerceUserService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/auth")
+public class EcommerceAuthController {
+    private final EcommerceUserService userService;
+    private final EcommerceTokenService tokenService;
+
+    public EcommerceAuthController(EcommerceUserService userService, EcommerceTokenService tokenService) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Principal login(@RequestBody NewLoginRequest req) {
+        if (req.getUsername() == null || req.getPassword() == null) {
+            throw new RuntimeException("Missing username or password");
+        }
+
+        EcommerceUser user = null;
+        try {
+            user = userService.loginUser(req);
+
+            if (!req.getPassword().equals(user.getPassword())) {
+                System.out.println(req.getPassword());
+                System.out.println(user.getPassword());
+                throw new Exception("Wrong password");
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
+
+        Principal principal = new Principal(user.getUserId(), user.getUsername());
+        String token = tokenService.createNewToken(principal);
+
+        return principal;
+    }
+}
