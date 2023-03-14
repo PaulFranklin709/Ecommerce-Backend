775fba105bd26511037d806d7d4fa4e91ef65423
Paul Franklin <paul219@revature.net>
2023-03-14 15:10:34 -0400

    Update EcommerceUser.java

--- src/main/java/revature/paulfranklin/ecommerce/model/EcommerceUser.java
+++ src/main/java/revature/paulfranklin/ecommerce/model/EcommerceUser.java
@@ -1,5 +1,52 @@
 package revature.paulfranklin.ecommerce.model;
 
-public class EcommerceUser {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table
+public class EcommerceUser {
+    @Id
+    private String userId;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public EcommerceUser() {
+    }
+
+    public EcommerceUser(String userId, String username, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
