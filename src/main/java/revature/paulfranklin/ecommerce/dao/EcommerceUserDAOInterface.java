a47289d64e2c8b7d7d0dcbd805062dfc4a023f7f
Paul Franklin <paul219@revature.net>
2023-03-14 16:46:56 -0400

    Update EcommerceUserDAOInterface.java

--- src/main/java/revature/paulfranklin/ecommerce/dao/EcommerceUserDAOInterface.java
+++ src/main/java/revature/paulfranklin/ecommerce/dao/EcommerceUserDAOInterface.java
@@ -2,8 +2,9 @@ package revature.paulfranklin.ecommerce.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.stereotype.Repository;
 import revature.paulfranklin.ecommerce.model.EcommerceUser;
 
+@Repository
 public interface EcommerceUserDAOInterface extends CrudRepository<EcommerceUser, String> {
-
 }
