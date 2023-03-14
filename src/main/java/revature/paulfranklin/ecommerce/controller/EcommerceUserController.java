822f86acd083d4104a0b8a66480e3c905fefa2f4
Paul Franklin <paul219@revature.net>
2023-03-14 12:58:41 -0400

    Create EcommerceUserController.java

---
+++ src/main/java/revature/paulfranklin/ecommerce/controller/EcommerceUserController.java
@@ -0,0 +1,41 @@
+package revature.paulfranklin.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.HttpStatus;
+
+import revature.paulfranklin.ecommerce.dtos.requests.NewUserRequest;
+import revature.paulfranklin.ecommerce.service.EcommerceTokenService;
+import revature.paulfranklin.ecommerce.service.EcommerceUserService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
+public class EcommerceUserController {
+    private final EcommerceTokenService tokenService;
+    private final EcommerceUserService userService;
+
+    public EcommerceUserController(EcommerceTokenService tokenService, EcommerceUserService userService) {
+        this.tokenService = tokenService;
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void newUser(@RequestBody NewUserRequest req) {
+        if (req.getUsername() == null || req.getPassword() == null) {
+            throw new RuntimeException("Missing username or password");
+        }
+
+        try {
+            userService.newUser(req);
+        } catch (Exception exception) {
+        	throw new RuntimeException("Did not create user");
+        }
+    }
+}
