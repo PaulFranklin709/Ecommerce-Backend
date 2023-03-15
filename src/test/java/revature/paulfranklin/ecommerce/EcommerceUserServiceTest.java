184edeae010a6cce3645d4791d98ecd3a1f990ed
Paul Franklin <paul219@revature.net>
2023-03-15 14:24:42 -0400

    Create EcommerceUserServiceTest.java

---
+++ src/test/java/revature/paulfranklin/ecommerce/EcommerceUserServiceTest.java
@@ -0,0 +1,18 @@
+package revature.paulfranklin.ecommerce;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EcommerceUserServiceTest {
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void test_newUser_givenNewUserRequest() {
+    }
+
+    @Test
+    public void test_findUser_givenUsername() {
+    }
+}
