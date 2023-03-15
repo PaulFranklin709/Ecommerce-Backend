62d600e67324b9470b1458889e8f94aa076b13d3
Paul Franklin <paul219@revature.net>
2023-03-15 15:22:42 -0400

    Update EcommerceOrderServiceTest.java

--- src/test/java/revature/paulfranklin/ecommerce/service/EcommerceOrderServiceTest.java
+++ src/test/java/revature/paulfranklin/ecommerce/service/EcommerceOrderServiceTest.java
@@ -2,10 +2,16 @@ package revature.paulfranklin.ecommerce.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import revature.paulfranklin.ecommerce.dao.EcommerceOrderDAOInterface;
 
 public class EcommerceOrderServiceTest {
+    private EcommerceOrderService sut;
+    private final EcommerceOrderDAOInterface mockOrderRepo = Mockito.mock(EcommerceOrderDAOInterface.class);
+
     @Before
     public void init() {
+        sut = new EcommerceOrderService(mockOrderRepo);
     }
 
     @Test
