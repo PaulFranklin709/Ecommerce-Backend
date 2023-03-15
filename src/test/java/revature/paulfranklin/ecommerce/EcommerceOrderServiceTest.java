16f71c519513fced56dd1a93c476abf26ecea33e
Paul Franklin <paul219@revature.net>
2023-03-15 14:29:00 -0400

    Update EcommerceOrderServiceTest.java

--- src/test/java/revature/paulfranklin/ecommerce/EcommerceOrderServiceTest.java
+++ src/test/java/revature/paulfranklin/ecommerce/EcommerceOrderServiceTest.java
@@ -1,4 +1,14 @@
 package revature.paulfranklin.ecommerce;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class EcommerceOrderServiceTest {
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void test_createOrder_givenNewOrderRequestAndEcommerceUser() {
+    }
 }
