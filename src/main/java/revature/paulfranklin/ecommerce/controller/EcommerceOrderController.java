fc1a8e00fa347491857dac4a2ae39c1a8c0e158b
Paul Franklin <paul219@revature.net>
2023-03-15 13:00:50 -0400

    Update EcommerceOrderController.java

--- src/main/java/revature/paulfranklin/ecommerce/controller/EcommerceOrderController.java
+++ src/main/java/revature/paulfranklin/ecommerce/controller/EcommerceOrderController.java
@@ -1,11 +1,15 @@
 package revature.paulfranklin.ecommerce.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import revature.paulfranklin.ecommerce.dtos.requests.NewOrderRequest;
+import revature.paulfranklin.ecommerce.dtos.responses.Principal;
+import revature.paulfranklin.ecommerce.model.EcommerceUser;
 import revature.paulfranklin.ecommerce.service.EcommerceOrderService;
 import revature.paulfranklin.ecommerce.service.EcommerceTokenService;
 import revature.paulfranklin.ecommerce.service.EcommerceUserService;
+
+import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin
 @RestController
@@ -19,5 +23,21 @@ public class EcommerceOrderController {
         this.tokenService = tokenService;
         this.userService = userService;
         this.orderService = orderService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createOrder(@RequestBody NewOrderRequest req, HttpServletRequest servletReq) {
+        String token = servletReq.getHeader("authorization");
+        if (token == null || token.isEmpty()) throw new RuntimeException("Missing token");
+
+        if (req.getTotalPrice() == null) {
+            throw new RuntimeException("Missing total price");
+        }
+
+        Principal principal = tokenService.retrievePrincipalFromToken(token);
+        EcommerceUser user = userService.findUserById(principal.getUserId());
+
+        orderService.createOrder(req, user);
     }
 }
