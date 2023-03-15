d2fabec3541194105031d9efb7ea9ac4265911c2
Paul Franklin <paul219@revature.net>
2023-03-15 12:42:37 -0400

    Update EcommerceOrderService.java

--- src/main/java/revature/paulfranklin/ecommerce/service/EcommerceOrderService.java
+++ src/main/java/revature/paulfranklin/ecommerce/service/EcommerceOrderService.java
@@ -2,6 +2,11 @@ package revature.paulfranklin.ecommerce.service;
 
 import org.springframework.stereotype.Service;
 import revature.paulfranklin.ecommerce.dao.EcommerceOrderDAOInterface;
+import revature.paulfranklin.ecommerce.dtos.requests.NewOrderRequest;
+import revature.paulfranklin.ecommerce.model.EcommerceOrder;
+import revature.paulfranklin.ecommerce.model.EcommerceUser;
+
+import java.util.UUID;
 
 @Service
 public class EcommerceOrderService {
@@ -9,5 +14,10 @@ public class EcommerceOrderService {
 
     public EcommerceOrderService(EcommerceOrderDAOInterface orderRepository) {
         this.orderRepository = orderRepository;
+    }
+
+    public void createOrder(NewOrderRequest req, EcommerceUser user) {
+        EcommerceOrder order = new EcommerceOrder(UUID.randomUUID().toString(), user, req.getTotalPrice());
+        orderRepository.save(order);
     }
 }
