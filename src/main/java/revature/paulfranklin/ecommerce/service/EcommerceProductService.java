60554c3e1f1cdf0bff0e2c7ea5b17c61b7c6c9c7
Paul Franklin <paul219@revature.net>
2023-03-16 13:35:38 -0400

    Update EcommerceProductService.java

--- src/main/java/revature/paulfranklin/ecommerce/service/EcommerceProductService.java
+++ src/main/java/revature/paulfranklin/ecommerce/service/EcommerceProductService.java
@@ -18,8 +18,8 @@ public class EcommerceProductService {
     public ArrayList<EcommerceProduct> findAllProducts() {
         ArrayList<EcommerceProduct> products = productRepository.findAll();
         if (products.size() == 0) {
-            products.add(new EcommerceProduct(UUID.randomUUID().toString(), "First Product", "First Product Description", 1.00));
-            products.add(new EcommerceProduct(UUID.randomUUID().toString(), "Second Product", "Second Product Description", 1.01));
+            products.add(new EcommerceProduct("4d100ac4-cf71-411c-95dc-1edbd64541ba", "First Product", "First Product Description", 1.00));
+            products.add(new EcommerceProduct("321b8676-d546-4a5b-bd4f-684bdba39803", "Second Product", "Second Product Description", 1.01));
         }
         return products;
     }
